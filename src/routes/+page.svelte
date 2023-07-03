d59c820e9008e68d37f0b93386c3d85a2316b220
DEVELOPER JETORBIT <jet-dev@MacBook-Pro-DEVELOPER.local>
2023-07-03 14:44:46 +0700

    update for this

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,59 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+// @ts-nocheck
+
+    import anime from 'animejs/lib/anime.es.js';
+	import { onMount } from 'svelte';
+
+    onMount(function(params) {
+      
+    });
+    const goBig = () => {
+        anime({
+            targets: '.flex .dot',
+            translateX: function(el) {
+                return el.getAttribute('data-x');
+            },
+            translateY: function(el, i) {
+                return 50 + (-50 * i);
+            },
+            scale: function(el, i, l) {
+                return (l - i) + .25;
+            },
+            rotate: function() { return anime.random(-360, 360); },
+            borderRadius: function() { return ['50%', anime.random(10, 35) + '%']; },
+            duration: function() { return anime.random(1200, 1800); },
+            delay: function() { return anime.random(0, 400); },
+            direction: 'alternate',
+            loop: true
+        });
+    }
 </script>
+<div class="grid h-screen place-items-center">
+    <div on:mouseover={goBig} on:focus={goBig} class="flex text-center">
+        <div data-x="170" class="dot">
+            R
+        </div>
+        <div data-x="170" class="dot">
+            N
+        </div>
+        <div data-x="170" class="dot">
+            S
+        </div>
+    </div>
+</div>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+    .dot{
+        pointer-events: none;
+        position: relative;
+        width: 18px;
+        height: 18px;
+        margin: 1px;
+        background-color: currentColor;
+        font-size: 14px;
+        color: #ffffff;
+        background: #18FF74;
+    }
 </style>
